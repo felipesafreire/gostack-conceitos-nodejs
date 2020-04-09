f062bb534089f03157c9923e87c7c93749c78653
Felipe Sá Freire <felipe.freire@fabricadecodigos.com.br>
2020-04-09 12:12:39 -0300

    Aplicado regras nos metodos.

--- src/app.js
+++ src/app.js
@@ -1,33 +1,76 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-const repositories = [];
+let repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const newRepo = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(newRepo);
+  return response.json(newRepo);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const id = request.params.id;
+  const { title, url, techs } = request.body;
+
+  const repo = repositories.find((repo) => repo.id == id);
+
+  if (!repo) {
+    return response.status(400).json({ message: "Repository not found." });
+  }
+
+  repo.title = title;
+  repo.url = url;
+  repo.techs = techs;
+
+  return response.json(repo);
 });
 
-app.delete("/repositories/:id", (req, res) => {
-  // TODO
+app.delete("/repositories/:id", (request, response) => {
+  const id = request.params.id;
+  const repo = repositories.find((repo) => repo.id == id);
+
+  if (!repo) {
+    return response.status(400).json({ message: "Repository not found." });
+  }
+
+  repositories = repositories.filter((repository) => repository.id != repo.id);
+
+  return response.status(204).json(repositories);
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const id = request.params.id;
+
+  const repo = repositories.find((repo) => repo.id == id);
+
+  if (!repo) {
+    return response.status(400).json({ message: "Repository not found." });
+  }
+
+  repo.likes += 1;
+
+  return response.json(repo);
 });
 
 module.exports = app;
